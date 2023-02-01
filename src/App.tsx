5cb996fd149a037a3177b429bdef4345d9000535
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2023-02-01 15:44:40 +0530

    nav bar ui changes
    changed alignment and spacing

--- src/App.tsx
+++ src/App.tsx
@@ -53,37 +53,38 @@ export default function App() {
       ) : (
         <div className="bg-white overflow-x-hidden h-fit">
           <section className="fixed flex items-center z-50 lg:justify-center lg:left-0 lg:h-full w-full h-24 md:h-full md:w-[200px]">
-            <div className="relative bg-white md:bg-transparent w-full flex h-24 flex-col md:h-screen ">
-              <div className="absolute right-5 md:right-0 md:top-10 md:relative flex items-center md:block h-24 z-10 md:h-80 ">
-                <ul className="text-center text-midcoffee md:mt-64 space-x-5 font-raleway flex md:-rotate-90 h-fit">
-                  <li
-                    onClick={scrollContact}
-                    className="cursor-pointer flex items-center px-1 hover:italic duration-300"
-                  >
-                    CONTACT
-                  </li>
-                  <li
-                    onClick={scrollWork}
-                    className="cursor-pointer flex items-center px-1 hover:italic duration-300"
-                  >
-                    PROJECTS
-                  </li>
-                  <li
-                    onClick={scrollSkills}
-                    className="cursor-pointer flex items-center px-1 hover:italic duration-300"
-                  >
-                    SKILLS
-                  </li>
-                  <li
-                    onClick={scrollAbout}
-                    className="cursor-pointer flex items-center px-1 hover:italic duration-300"
-                  >
-                    ABOUT
-                  </li>
-                </ul>
+            <div className="relative bg-white md:bg-transparent w-full md:w-[200px] flex items-center justify-between md:justify-center h-24 md:h-screen ">
+              <div className="w-1/6 md:absolute md:mx-auto md:top-0 md:w-full md:h-24 md:max-w-full max-w-[6rem] flex items-center justify-center">
+                <img src={logo} alt="logo" className="w-14" />
               </div>
+              <ul className="md:absolute w-5/6 max-w-[20rem] md:max-w-[26rem] md:h-[200px] h-full md:w-screen text-center md:-rotate-90 text-midcoffee justify-between font-raleway flex">
+                <li
+                  onClick={scrollContact}
+                  className="cursor-pointer flex items-center px-1 hover:italic duration-300"
+                >
+                  CONTACT
+                </li>
+                <li
+                  onClick={scrollWork}
+                  className="cursor-pointer flex items-center px-1 hover:italic duration-300"
+                >
+                  PROJECTS
+                </li>
+                <li
+                  onClick={scrollSkills}
+                  className="cursor-pointer flex items-center px-1 hover:italic duration-300"
+                >
+                  SKILLS
+                </li>
+                <li
+                  onClick={scrollAbout}
+                  className="cursor-pointer flex items-center px-1 hover:italic duration-300"
+                >
+                  ABOUT
+                </li>
+              </ul>
               <div className=" md:visible invisible absolute bottom-0 w-full h-40 text-center">
-                <h1 className="text-lightcoffee md:-rotate-90 md:mt-10 select-none cursor-default">
+                <h1 className="text-lightcoffee md:-rotate-90 md:mt-20 select-none cursor-default">
                   @2023
                 </h1>
               </div>
